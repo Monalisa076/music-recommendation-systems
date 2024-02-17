b73642c748c7c60cd3665e08742cd70d84bc7ee9
Monalisa076 <129836610+Monalisa076@users.noreply.github.com>
2024-02-17 20:13:56 +0530

    Add files via upload

---
+++ music.py
@@ -0,0 +1,9 @@
+
+# first import the module
+import webbrowser
+  
+# then make a url variable
+url = "https://youtu.be/dodDteh-m3Q"
+  
+# then call the default open method described above
+webbrowser.open(url)
